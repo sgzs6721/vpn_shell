99a42a854b60a3271509e8636605bbc109e2587c
Zhongshan Shangguan <sgzs6721@126.com>
2015-11-23 10:13:40 +0800

    mark pptp && add daocloud

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -2,12 +2,12 @@ git clone https://github.com/sgzs6721/docker-shadowsocks.git
 cd docker-shadowsocks
 docker build -t oddrationale/docker-shadowsocks .
 docker run -d -p 1984:1984 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 1984 -k 12345678 -m aes-256-cfb
-cd ..
-git clone https://github.com/sgzs6721/docker-vpn-pptp.git
-cd docker-vpn-pptp
-docker build -t mobtitude/vpn-pptp .
-echo "sgzs6721      *           1qazxsw2    *" > chap-secrets
-docker run -d --privileged --net=host -v /root/docker-vpn-pptp/chap-secrets:/etc/ppp/chap-secrets mobtitude/vpn-pptp
+#cd ..
+#git clone https://github.com/sgzs6721/docker-vpn-pptp.git
+#cd docker-vpn-pptp
+#docker build -t mobtitude/vpn-pptp .
+#echo "sgzs6721      *           1qazxsw2    *" > chap-secrets
+#docker run -d --privileged --net=host -v /root/docker-vpn-pptp/chap-secrets:/etc/ppp/chap-secrets mobtitude/vpn-pptp
 
 
 
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 chmod 755 vpn_shell/shadowsocks.sh
 vpn_shell/shadowsocks.sh
+curl -sSL https://get.daocloud.io/daomonit/install.sh | sh -s 8dfe204d7fb1b8fe458a54db571eed6da2c42716
